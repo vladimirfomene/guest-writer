1930a75be9348498bcaed7ea139eafc40fcffb95
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-06 18:32:00 -0200

    saving writer details locally

--- src/index.js
+++ src/index.js
@@ -3,24 +3,32 @@ const {promisify} = require('util');
 const readline = require('readline');
 const writeFile = promisify(fs.writeFile);
 
-// for now it just creates new a article based on a few questions
+let writer;
+const writerDetailsPath = `${__dirname}/../.writer.json`;
+if (fs.existsSync(writerDetailsPath)) {
+    writer = JSON.parse(fs.readFileSync(writerDetailsPath, 'utf8'));
+}
+
 newArticle();
 
 async function newArticle() {
     console.log('=====================================================');
     console.log('Howdy! To create a new article for you, I just have to ask you a few questions.');
-    console.log('Let\'s start!');
-    console.log('');
-    const authorName = await question('What is your name?');
-    const authorTwitter = await question('What is your twitter (e.g. brunoskrebs)?');
-    const authorEmail = await question('What is your public email?');
-    const date = new Date().toISOString().split('T')[0];
 
-    const blackBgColor = '#000';
+    if (!writer) {
+        console.log('Let\'s start!');
+        console.log('');
+        const name = await question('What is your name?');
+        const twitter = await question('What is your twitter (e.g. brunoskrebs)?');
+        const email = await question('What is your public email?');
 
-    console.log('');
-    console.log('Great! Now, please, answer the following about the article:');
-    console.log('');
+        writer = {name, twitter, email};
+        await writeFile(writerDetailsPath, JSON.stringify(writer));
+
+        console.log('');
+        console.log('Great! Now, please, answer the following about the article:');
+        console.log('');
+    }
 
     const articleTitle = await question('What is the title of the article?');
     const articleDescription = await question('What is its description?');
@@ -29,11 +37,11 @@ async function newArticle() {
     console.log('Amazing! Let me create the article for you.');
     console.log('');
 
-
+    const date = new Date().toISOString().split('T')[0];
     let template = fs.readFileSync(`${__dirname}/template.markdown`, 'utf8');
-    template  = template.replace(/AUTHOR.NAME/g, authorName)
-        .replace(/AUTHOR.TWITTER/g, authorTwitter)
-        .replace(/AUTHOR.EMAIL/g, authorEmail)
+    template  = template.replace(/AUTHOR.NAME/g, writer.name)
+        .replace(/AUTHOR.TWITTER/g, writer.twitter)
+        .replace(/AUTHOR.EMAIL/g, writer.email)
         .replace(/ARTICLE.TITLE/g, articleTitle)
         .replace(/ARTICLE.DESCRIPTION/g, articleDescription)
         .replace(/ARTICLE.DATE/g, date);
